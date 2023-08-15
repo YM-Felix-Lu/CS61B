f0a4a35e5c3f083dab56a10b60844420e63cace7
Felix Lu <1712022018@qq.com>
2023-08-15 22:26:51 +0800

    lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,15 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Felix
  */
 public class LeapYear {
+
+    /** Return whether a year is a leap year. */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return True
+        }
+        return False
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
